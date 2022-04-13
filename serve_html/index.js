113aae69fba096295d2bc09f059d121d7d6b9718
yasserkassim <kassim.yasser@gmail.com>
2022-04-12 22:44:22 -0400

    Update index.js

--- serve_html/index.js
+++ serve_html/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 const uri = process.env.ATLAS_URI;
-//console.log(uri);
-mongoose.connect(uri, { useUnifiedTopology: true }
-);
+
+mongoose.connect(uri, { useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once("open", () => {
